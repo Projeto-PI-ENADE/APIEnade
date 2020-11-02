b03ffcc017f531766e3ac5ed50509652f050b1bf
Ruannilton <ruannilton@gmail.com>
2020-11-02 17:33:04 -0300

    clean

--- src/Services/Exportador.ts
+++ src/Services/Exportador.ts
@@ -1,16 +1,12 @@
-import { ExportToCsv } from "export-to-csv";
 const ObjectsToCsv = require('objects-to-csv');
 
 
-export default async function ExportarCSV(path:string, list:Array<Object>)
-{
-  const csv =  new ObjectsToCsv(list);
+export default async function ExportarCSV(path: string, list: Array<Object>) {
+  const csv = new ObjectsToCsv(list);
 
-  try
-  {
+  try {
     await csv.toDisk(path);
-  }catch(err)
-  {
+  } catch (err) {
     console.log(err);
   }
 
