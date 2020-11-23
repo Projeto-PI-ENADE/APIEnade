0555720ff8ea5946547e8696c419a3e308c4837a
Lucas Souza <souzacontato18@gmail.com>
2020-11-23 10:04:49 -0300

    bugfix Ranking notas
    corrigido erro no ranking das notas
    cade a Tete?

---
+++ src/Services/IntendificadorCurso.ts
@@ -0,0 +1,32 @@
+import cursos2018 from './data/cursos_2018.json';
+
+interface IDadosCurso
+{
+    curso_id,
+    nome_curso,
+    count
+}
+
+interface IEntrada
+{
+    curso_id,
+    count
+}
+
+
+function DataDocumentCursos(list:Array<IEntrada>)
+{
+    
+
+
+}
+
+
+export default DataDocumentCursos;
+
+
+
+
+
+
+
