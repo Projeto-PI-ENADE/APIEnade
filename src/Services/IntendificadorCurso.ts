0241b9c78ad51bda81814da6c798c353b04199a8
Lucas Souza <souzacontato18@gmail.com>
2020-11-27 16:39:33 -0300

    JSON DO CARALHO
    TYPESCRIPT NAO FAZ SENTIDO, TIPEI COMO BOOLEAN, RECEBI COMO BOOLEAN, MAS ESSE CARALHO DEFINICIU COMO STRING, PORRA, SE VAI DEFINIR COM OUTRO VALOR, PELO MENOS AVISA QUE A TIPAGEM NAO FUNCIONA MANO, 40 ANOS DE CURSO PARA ISSO

--- src/Services/IntendificadorCurso.ts
+++ src/Services/IntendificadorCurso.ts
@@ -1,40 +1,33 @@
-import * as cursos2018 from './data/cursos_2018.json';
 
-
-interface IDadosCurso
+interface ITipoCurso
 {
-    curso_id:number,
-    nome_curso:string,
-    count:number
+    id_curso:number
+    nome_curso:string
 }
+ 
+//    const index = Object.keys(cursos2018).find(key => key == pos)
 
-interface IEntrada
-{
-    curso_id:number,
-    count:number
+
+export default {
+
+    async IndentificarPorID(ano:string)
+    {
+        const url = './data/cursos_'+ano;
+        const file = await import(url);
+        return file;
+    },
+
+    async IndentificarPorTipo(ano:string)
+    {
+        const url = './data/cursos_'+ano+'_por_tipo.json';
+        const file = await import(url);
+        return file;
+    }
+
+
+
+
 }
-
-
-export default function DataDocumentCursos(list:Array<IEntrada>):Array<IDadosCurso>
-{
-    let data =  new Array<IDadosCurso>();
-    console.log(cursos2018);
-    list.forEach(element => {
-        const pos = element.curso_id.toString(); 
-        const index = Object.keys(cursos2018).find(key => key == pos)
-        console.log(index);
-        let temp:IDadosCurso = 
-        {
-            curso_id:element.curso_id,
-            count:element.count,
-            nome_curso: cursos2018[index],
-        }
-        console.log(temp);
-        data.push(temp)
-    });
-    return data;
-}
-
 
 
 
